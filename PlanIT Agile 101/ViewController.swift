bc309fa522b551796e96225e16a4f16fb7fba271
lucy.french <lucyisabellefrench@gmail.com>
2016-05-19 19:22:26 +1200

    Designed sign in page. Not functioning yet but is transitioning to/from log in page.

--- PlanIT Agile 101/ViewController.swift
+++ PlanIT Agile 101/ViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var EmailTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    
+    //MARK: Actions
+    @IBAction func LogInButton(sender: UIButton) {
+        if let text = EmailTextField.text {
+            print(text)
+            print(PasswordTextField.text)
+        }
+        
+    }
+    
+    @IBAction func GoSignUpButton(sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        EmailTextField.delegate = self
+        PasswordTextField.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
