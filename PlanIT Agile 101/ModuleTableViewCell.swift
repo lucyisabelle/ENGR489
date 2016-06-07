336f7cc343ddc92317099c9bb421f535869a1c3d
lucy.french <lucyisabellefrench@gmail.com>
2016-06-07 20:40:00 +1200

    Added landing page

---
+++ PlanIT Agile 101/ModuleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ModuleTableViewCell.swift
+//  PlanIT Agile 101
+//
+//  Created by Lucy French on 6/06/16.
+//  Copyright © 2016 Lucy French. All rights reserved.
+//
+
+import UIKit
+
+class ModuleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
