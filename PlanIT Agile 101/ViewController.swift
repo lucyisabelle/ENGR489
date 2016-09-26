03e8575c3f6b74a59a570835d63e52029644a34c
lucy.french <lucyisabellefrench@gmail.com>
2016-09-26 21:11:28 +1300

    Only logs in if it's a plan it email address.

--- PlanIT Agile 101/ViewController.swift
+++ PlanIT Agile 101/ViewController.swift
@@ -12,69 +12,63 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     //MARK: Properties
     @IBOutlet weak var EmailTextField: UITextField!
-    @IBOutlet weak var PasswordTextField: UITextField!
-    var textField = "Hello"
+    var loginSuccess = false
+
     
     //MARK: Actions
     @IBAction func LogInButton(sender: UIButton) {
-        if let text = EmailTextField.text {
-            print(text)
-            print(PasswordTextField.text)
+        
+        let text = EmailTextField.text!
+        if text.lowercaseString.rangeOfString("@planit.co.nz") != nil {
+            print("exists")
+            loginSuccess = true
+            //performSegueWithIdentifier("segueTest", sender: self)
         }
         
     }
     
-    @IBAction func GoSignUpButton(sender: UIButton) {
-    }
     
-    var databasePath = NSString()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        EmailTextField.delegate = self
-        PasswordTextField.delegate = self
-        
-        let filemgr = NSFileManager.defaultManager()
-        let dirPaths = filemgr.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
-        
-        databasePath = dirPaths[0].URLByAppendingPathComponent("agileDB.db").path!
-        
-        print(databasePath)
-        
-        
-        if !filemgr.fileExistsAtPath(databasePath as String) {
-            print("Got into original filemgr")
-            let agileDB = FMDatabase(path: databasePath as String)
-            
-            if agileDB == nil {
-                print("Error: \(agileDB.lastErrorMessage())")
-            }
-            
-            if agileDB.open() {
-                let sql_stmt = "CREATE TABLE IF NOT EXISTS USERS (EMAIL CHAR(50) PRIMARY KEY NOT NULL, PASSWORD CHAR(20) NOT NULL);"
-                if !agileDB.executeStatements(sql_stmt) {
-                    print("Error: \(agileDB.lastErrorMessage())")
-                }
-                agileDB.close()
-            } else {
-                print("Error: \(agileDB.lastErrorMessage())")
-            }
-        }
-        
+        // Do any additional setup after loading the view, typically from a nib
+        self.EmailTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String?, sender: AnyObject?) -> Bool {
+        if let ident = identifier {
+            if ident == "segueTest" {
+                if loginSuccess != true {
+                    return false
+                }
+            }
+        }
+        return true
+    }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
         if (segue.identifier == "segueTest") {
-            let navController = segue.destinationViewController as! UINavigationController
-            let detailController = navController.topViewController as! ModuleTableViewController
+            let text = EmailTextField.text!
+            if text.lowercaseString.rangeOfString("@planit.co.nz") != nil {
+                print("Performing segue")
+                let navController = segue.destinationViewController as! UINavigationController
+                let detailController = navController.topViewController as! ModuleTableViewController
             
-            detailController.toPass = databasePath
+                var databasePath = NSString()
+            
+                let filemgr = NSFileManager.defaultManager()
+                let dirPaths = filemgr.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+                
+                databasePath = dirPaths[0].URLByAppendingPathComponent("agileDB.db").path!
+            
+                detailController.toPass = databasePath
+                //return true
+            }
             
         }
     }
